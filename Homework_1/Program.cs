8e47fcc99218fdde61ef777e87abf98dcf7e1da5
Horvi <48880572+HKrisz@users.noreply.github.com>
2021-09-12 19:09:19 +0200

    Add files via upload

---
+++ Homework_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[8] { 1, 6, 8, 4, 5, 7, 2, 3 };
+
+            int maxNumber = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxNumber)
+                {
+                    maxNumber = numbers[i];
+                }
+            }
+
+
+            Console.Write($"A legnagyobb szám a tömbben: {maxNumber}");
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
